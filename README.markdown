65bb3e60d2505bef97ad6fb217353b83e3f7293d
Collin Stedman <cstedman@princeton>
2015-03-13 03:34:09 -0400

    Initial commit

---
+++ README.markdown
@@ -0,0 +1,7 @@
+GoGoTo
+===
+
+Tab through Google search results, just like the old native experience
+---
+
+By Collin Stedman
---
+++ script.js
@@ -0,0 +1,4 @@
+var results = document.getElementsByClassName('r');
+for (var i = 0; i < results.length; i++) {
+  results[i].tabIndex = 999;
+}
