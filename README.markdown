2e5ef9ab3108764dc10913c9e9d945b284738cf2
Collin Stedman <cstedman@princeton>
2015-03-14 00:42:05 -0400

    Just need the icons

--- README.markdown
+++ README.markdown
@@ -1,7 +1,12 @@
 GoGoTo
 ===
 
-Tab through Google search results, just like the old native experience
+###Tab through Google search results
+####(Just like the old native experience)
 ---
 
-By Collin Stedman
+If you're like me (a die-hard Vimmer), then you abhor the idea of needing to touch your mouse. Google used to be good to us, allowing users to tab through search results and open them with the enter key. Then, without warning, Google yanked the feature. At first, I was distraught. Then I decided to put it back.
+
+GoGoTo is a tiny Chrome extension (<3KB) that replicates the old Google tab-through-results experience exactly as I remember it. No frills, just easy searching. Keyboard is king.
+
+-CS
--- script.js
+++ script.js
@@ -1,4 +1,55 @@
-var results = document.getElementsByClassName('r');
-for (var i = 0; i < results.length; i++) {
-  results[i].tabIndex = 999;
-}
+(function() {
+  var searchbar = document.getElementsByName('q')[0];
+  searchbar.tabIndex = 2;
+  var results = document.getElementsByClassName('r');
+  var anchors = [];
+  var arrows = [];
+  var size = null;
+  var color = null;
+  var activated = false;
+  for (var i = 0; i < results.length; i++) {
+    var anchor = results[i].children[0];
+    anchors.push(anchor);
+    if (i === 0) {
+      var style = window.getComputedStyle(results[i], null).getPropertyValue('height');
+      size = parseFloat(style.slice(0, -2))/2;
+      color = window.getComputedStyle(anchor, null).getPropertyValue('color');
+    } 
+    var div = document.createElement("div");
+    div.style.width = 0;
+    div.style.height = 0;
+    div.style.borderTop = (size * .866) + "px solid transparent";
+    div.style.borderBottom = (size * .866) + "px solid transparent";
+    div.style.borderLeft = size + "px solid " + color;
+    div.style.marginLeft = (-2 * size) + "px";
+    div.style.float = "left";
+    div.style.display = "none";
+    results[i].style.overflow = "visible";
+    results[i].appendChild(div);
+    arrows.push(div);
+  }
+  function createHandlers(i) {
+    return [function() { if (activated) arrows[i].style.display = "block"; },
+            function() { arrows[i].style.display = "none"; }];
+  }
+  for (var i = 0; i < anchors.length; i++) {
+    anchors[i].tabIndex = 1;
+    var funcs = createHandlers(i);
+    anchors[i].onfocus = funcs[0];
+    anchors[i].onblur = funcs[1];
+  }
+  document.onkeydown = function(e) {
+    e = e || window.event;
+    var key = e.which || e.charCode || e.keyCode;
+    if (key === 9) {
+      activated = true;
+    }
+  };
+  document.onkeyup = function(e) {
+    e = e || window.event;
+    var key = e.which || e.charCode || e.keyCode;
+    if (key === 9) {
+      activated = false;
+    }
+  };
+})();
